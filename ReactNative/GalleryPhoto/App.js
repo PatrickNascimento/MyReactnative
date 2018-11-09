6cf95e47610c951ba7c111b85ba4fd794c546f35
psn1462 <psn1462@gmail.com>
2018-11-09 13:29:06 -0200

    galeri de fotos

--- ReactNative/GalleryPhoto/App.js
+++ ReactNative/GalleryPhoto/App.js
@@ -1,21 +1,40 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+console.debug = console.debug || console.log;
+import React, { Component } from 'react';
+import { AppRegistry, StyleSheet, Text, View, TouchableOpacity, AlertIOS } from 'react-native';
+import AlbumList from './album-list';
+import { Scene, Router } from 'react-native-router-flux';
+import CameraRollPicker from 'react-native-camera-roll-picker';
+import { Actions } from 'react-native-router-flux'
 
-export default class App extends React.Component {
+
+export default class Example extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      albumEditMode: false
+    };
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Router>
+        <Scene key="root">
+          <Scene
+            key="albumList"
+            component={AlbumList}
+            title="Album" />
+          <Scene key="cameraRollPicker" component={CameraRollPicker} title="Bilder" />
+        </Scene>
+      </Router>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    flex: 1
+  }
 });
+
+
